0ef9cf51cb592d53612263dc7508125d3e399532
Ming Fang <mfang0126@gmail.com>
2023-07-17 00:22:18 +1200

    Add comments and treat octet-stream as pptx for now.

--- services/utils/process_file.py
+++ services/utils/process_file.py
@@ -1,19 +1,20 @@
+"process_file.py"
+
 import os
-from fastapi import HTTPException
+import hashlib
+import mimetypes
+import csv
 from io import BufferedReader
 from typing import Optional
-from fastapi import UploadFile
-import mimetypes
-from PyPDF2 import PdfReader
-import hashlib
-import docx2txt
-import csv
 import pptx
-
+import docx2txt
+from PyPDF2 import PdfReader
+from fastapi import UploadFile
 from models.models import Document
 
 
 async def get_document_from_file(file: UploadFile) -> Document:
+    "Get uploaded file as a Document object."
     extracted_text = await extract_text_from_form_file(file)
     doc = Document(text=extracted_text)
 
@@ -31,7 +32,7 @@ def extract_text_from_filepath(filepath: str, mimetype: Optional[str] = None) ->
         if filepath.endswith(".md"):
             mimetype = "text/markdown"
         else:
-            raise Exception("Unsupported file type")
+            raise ValueError(f"Unsupported file type: {mimetype}")
 
     # Open the file in binary mode
     file = open(filepath, "rb")
@@ -40,7 +41,22 @@ def extract_text_from_filepath(filepath: str, mimetype: Optional[str] = None) ->
     return extracted_text
 
 
+def extract_text_from_pptx(file: BufferedReader) -> str:
+    "Extract text from pptx using python-pptx"
+    extracted_text = ""
+    presentation = pptx.Presentation(file)
+    for slide in presentation.slides:
+        for shape in slide.shapes:
+            if shape.has_text_frame:
+                for paragraph in shape.text_frame.paragraphs:
+                    for run in paragraph.runs:
+                        extracted_text += run.text + " "
+                extracted_text += "\n"
+    return extracted_text
+
+
 def extract_text_from_file(file: BufferedReader, mimetype: str) -> str:
+    "extract text according to the mimetype"
     if mimetype == "application/pdf":
         # Extract text from pdf using PyPDF2
         reader = PdfReader(file)
@@ -67,20 +83,14 @@ def extract_text_from_file(file: BufferedReader, mimetype: str) -> str:
         mimetype
         == "application/vnd.openxmlformats-officedocument.presentationml.presentation"
     ):
-        # Extract text from pptx using python-pptx
-        extracted_text = ""
-        presentation = pptx.Presentation(file)
-        for slide in presentation.slides:
-            for shape in slide.shapes:
-                if shape.has_text_frame:
-                    for paragraph in shape.text_frame.paragraphs:
-                        for run in paragraph.runs:
-                            extracted_text += run.text + " "
-                    extracted_text += "\n"
+        extracted_text = extract_text_from_pptx(file)
+    elif mimetype == "application/octet-stream":
+        # if it's "application/octet-stream" we extract text as pptx for now
+        extracted_text = extract_text_from_pptx(file)
     else:
         # Unsupported file type
         file.close()
-        raise ValueError("Unsupported file type: {}".format(mimetype))
+        raise ValueError(f"Unsupported file type: {mimetype}")
 
     file.close()
     return extracted_text
@@ -96,20 +106,20 @@ async def extract_text_from_form_file(file: UploadFile):
     file_stream = await file.read()
 
     hash_code = hashlib.sha256(file_stream).hexdigest()
-    
+
     if not os.path.exists("./temp_files/"):
         os.makedirs("./temp_files/")
         print("Temporary Folder created successfully!")
-        
+
     temp_file_path = f"./temp_files/{hash_code}"
-    
+
     try:
         with open(temp_file_path, "wb") as f:
             f.write(file_stream)
         extracted_text = extract_text_from_filepath(temp_file_path, mimetype)
-        
-    except Exception as e:
-        raise e
+
+    except Exception as error:
+        raise error
 
     os.remove(temp_file_path)
 
